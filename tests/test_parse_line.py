79c063211fbf5d9c4ecd05e87e8529015c3ffd70
Albina <albinaa1122333@gmail.com>
2023-03-30 20:00:33 +0300

    functions for testing parse_line returned data

--- tests/test_parse_line.py
+++ tests/test_parse_line.py
@@ -1,3 +1,14 @@
 import pytest
 from main import parse_line
 
+def test_parse_line_only_name():
+    country_name, _, _ = parse_line("1 Ukraine 200km^2 40000000")
+    assert country_name == "Ukraine"
+
+def test_parse_line_category():
+    _, area, _, = parse_line("1 Ukraine 200km^2 40000000")
+    assert area == 200
+
+def test_parse_line_money() :
+    _, _, population = parse_line("1 Ukraine 200km^2 40000000")
+    assert population == 40000000
