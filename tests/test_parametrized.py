9d1eb249852a77939146965124f448b956d77040
Albina <albinaa1122333@gmail.com>
2023-03-30 20:15:32 +0300

    parametrized function for testing parse_line thrown errors

--- tests/test_parametrized.py
+++ tests/test_parametrized.py
@@ -8,3 +8,10 @@ from decimal import Decimal
                                         ])
 def test_parse_line_only_name(line, result):
     assert parse_line(line) == result
+
+@pytest.mark.parametrize("line, expected_exception", [("1 -200km^2 40000000", ValueError),
+                                                      ("1 200km^2 -40000000", ValueError),
+                                                      ("1", ValueError)])
+def test_parse_line_items_number(line , expected_exception):
+    with pytest.raises (expected_exception):
+        parse_line (line)
