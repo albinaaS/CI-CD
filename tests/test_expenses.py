59b5256ee5eb5eaf37de8ce087bfbe0066244a10
Albina <albinaa1122333@gmail.com>
2023-03-30 20:31:59 +0300

    test file reading function with area

--- tests/test_expenses.py
+++ tests/test_expenses.py
@@ -9,3 +9,12 @@ def test_total_by_population(prepare_text_file):
     assert set(expected.keys()) == set(result.keys())
     assert all([result[key] == expected[key] for key in result.keys()])
 
+def test_total_by_area(prepare_text_file):
+    expected = {"Usa": 9834000.0,
+                "France": 551695.0,
+                "Britain": 243610.0,
+                }
+    result = total_by_area(prepare_text_file)
+    assert set(expected.keys()) == set(result.keys())
+    assert all([result[key] == expected[key] for key in result.keys()])
+
